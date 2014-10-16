013adfadc5afa9f090e3988b3dac02e2765e7a37
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-17 00:35:54 +0200

    Adapted new settings design for apps
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Gallery.php
+++ Gallery.php
@@ -5,6 +5,7 @@ use Core\Lib\Amvc\App;
 
 /**
  * Gallery app
+ *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.d
  * @package App
  * @subpackage Gallery
@@ -13,6 +14,140 @@ use Core\Lib\Amvc\App;
  */
 class Gallery extends App
 {
+
+	// App uses language files
+	protected $language = true;
+
+	// App has a seperate css file
+	protected $css_file = true;
+
+	// App config
+	protected $config = [
+		'grid' => [
+			'group' => 'display',
+			'control' => 'select',
+			'data' => [
+				'array',
+				[
+					2,
+					3,
+					4,
+					6,
+					12
+				],
+				1
+			],
+			'default' => 4
+		],
+		'thumbnail_use' => [
+			'group' => 'thumbnail',
+			'control' => 'switch',
+			'default' => 1
+		],
+		'thumbnail_width' => [
+			'group' => 'thumbnail',
+			'control' => 'number',
+			'default' => 640
+		],
+		'thumbnail_quality' => [
+			'group' => 'thumbnail',
+			'control' => [
+				'number',
+				[
+					'min' => 1,
+					'max' => 100
+				]
+			],
+			'default' => 80
+		],
+		'path' => [
+			'group' => 'upload',
+			'control' => 'input',
+			'default' => '/Uploads/images/Gallery'
+		],
+		'upload_mime_types' => [
+			'group' => 'upload',
+			'control' => 'optiongroup',
+			'data' => [
+				'array',
+				[
+					'image/gif',
+					'image/jpeg',
+					'image/png',
+					'image/bmp'
+				],
+				1
+			]
+		]
+	];
+
+	protected $routes = [
+		[
+			'name' => 'album_index',
+			'route' => '/',
+			'ctrl' => 'album',
+			'action' => 'gallery'
+		],
+		[
+			'name' => 'album_album',
+			'route' => '/[i:id_album]',
+			'ctrl' => 'album',
+			'action' => 'index'
+		],
+		[
+			'name' => 'album_new',
+			'method' => 'GET|POST',
+			'route' => '/new',
+			'ctrl' => 'album',
+			'action' => 'edit'
+		],
+		[
+			'name' => 'album_edit',
+			'method' => 'GET|POST',
+			'route' => '/[i:id_album]/edit',
+			'ctrl' => 'album',
+			'action' => 'edit'
+		],
+		[
+			'name' => 'album_delete',
+			'method' => 'GET',
+			'route' => '[i:id_album]/delete',
+			'ctrl' => 'album',
+			'action' => 'delete'
+		],
+		[
+			'name' => 'picture',
+			'route' => '/picture/[i:id_picture]',
+			'ctrl' => 'picture',
+			'action' => 'index'
+		],
+		[
+			'name' => 'picture_edit',
+			'route' => '/picture/[i:id_picture]/edit',
+			'ctrl' => 'picture',
+			'action' => 'edit'
+		],
+		[
+			'name' => 'picture_random',
+			'route' => '/picture/random',
+			'ctrl' => 'picture',
+			'action' => 'random'
+		],
+		[
+			'name' => 'picture_upload',
+			'method' => 'GET|POST',
+			'route' => '/upload/[i:id_album]?',
+			'ctrl' => 'picture',
+			'action' => 'upload'
+		]
+	];
+
+	protected $permissions = [
+		'manage_album',
+		'manage_image',
+		'allow_upload'
+	];
+
 	protected function addPaths()
 	{
 		// fileupload dir and url
@@ -40,16 +175,16 @@ class Gallery extends App
 
 		$gallery_menu_buttons = [];
 
-		if ($album_list)
-		{
+		if ($album_list) {
 			$gallery_album_buttons = [];
 
-			foreach ($album_list as $album)
-			{
+			foreach ($album_list as $album) {
 				$gallery_album_buttons['gallery_' . FileIO::cleanFilename($album->title)] = [
 					'title' => $album->title,
-					'href' => Url::factory('gallery_album_album', ['id_album' => $album->id_album])->getUrl(),
-					'show' => true,
+					'href' => Url::factory('gallery_album_album', [
+						'id_album' => $album->id_album
+					])->getUrl(),
+					'show' => true
 				];
 			}
 
@@ -57,7 +192,7 @@ class Gallery extends App
 				'title' => $this->txt('album'),
 				'href' => Url::factory('gallery_album_index')->getUrl(),
 				'show' => true,
-				'sub_buttons' => $gallery_album_buttons,
+				'sub_buttons' => $gallery_album_buttons
 			];
 		}
 
@@ -73,27 +208,33 @@ class Gallery extends App
 		];
 		$gallery_menu_buttons['gallery_config'] = [
 			'title' => $this->txt('config'),
-			'href' => Url::factory('admin_app_config', ['app_name' => 'gallery'])->getUrl(),
+			'href' => Url::factory('admin_app_config', [
+				'app_name' => 'gallery'
+			])->getUrl(),
 			'show' => $this->checkAccess('gallery_manage_album')
 		];
 
 		$menu_buttons['gallery'] = [
 			'title' => $this->txt('headline'),
 			'href' => Url::factory('gallery_album_index')->getUrl(),
-			'show' => !empty($album_list) || $this->checkAccess('gallery_manage_album'),
+			'show' => ! empty($album_list) || $this->checkAccess('gallery_manage_album'),
 			'sub_buttons' => $gallery_menu_buttons
 		];
 	}
 
 	/**
 	 * Writes membername to the picture and gallery tables of the member to delete
+	 *
 	 * @param int $id_member
 	 */
 	public function onMemberDelete($id_member)
 	{
 		// Get the member name by member id
 		$model = App::getInstance('forum')->getModel('members');
-		$model->find($id_member, ['member_name', 'real_name']);
+		$model->find($id_member, [
+			'member_name',
+			'real_name'
+		]);
 		$member_name = $member->real_name ? $member->real_name : $member->member_name;
 
 		// Create Gallery app
--- Permissions.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('TEKFW'))
-	die('Can not run without CoreFramework');
-
-return [
-	'manage_album',
-	'manage_image',
-	'allow_upload'
-];
